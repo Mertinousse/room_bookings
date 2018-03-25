3af1b50bb9379bc501710425057658a92b6a7e42
Mertinousse <33594576+Mertinousse@users.noreply.github.com>
2018-03-26 01:10:32 +0200

    postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180321204327) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "bookings", force: :cascade do |t|
     t.string "room"
     t.date "start"
