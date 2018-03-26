d18aa000a02208a6d494d44abf788393c8e8b06d
Mertinousse <mertinousse@gmail.com>
2018-03-26 22:21:25 +0200

    validation fix

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,8 +3,7 @@ class ApiController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def info
-    data = params.require(:booking).permit(:room, :date)
-    if data.has_key? :room and data.has_key? :date
+    if booking_present? and data.has_key? :room and data.has_key? :date
       data[:date] = Date.parse(data[:date])
       @resp = { :booked => false }
       Booking.all.each do |b|
@@ -17,6 +16,16 @@ class ApiController < ApplicationController
       @resp = { "error" => "JSON validation failed" }
     end
     render :json => @resp
+  end
+
+  private
+  def booking_present?
+    begin
+      @data = params.require(:booking).permit(:room, :date)
+    rescue ActionController::ParameterMissing => e
+      return false
+    end
+    return true
   end
 
 end
