2b1d65b46d3d11a7af6c09dc5b71ca8e2db56279
Mertinousse <mertinousse@gmail.com>
2018-03-28 01:21:24 +0200

    asd

--- test/system/bookings_test.rb
+++ test/system/bookings_test.rb
@@ -3,9 +3,13 @@ require "application_system_test_case"
 class BookingsTest < ApplicationSystemTestCase
   test "creating a booking" do
     visit bookings_path
+    puts "1"
     click_on "new-booking"
+    puts "2"
     fill_in "booking[room]", with: "001"
+    puts "3"
     click_on "commit"
+    puts "4"
     assert_selector ".border-danger .card-text", text: "Start is not a valid date"
   end
 end
