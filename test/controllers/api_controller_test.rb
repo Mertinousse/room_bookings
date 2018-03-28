0d819e184ad976153ffe2383b9cbf54cc5980b27
Mertinousse <mertinousse@gmail.com>
2018-03-28 02:14:32 +0200

    api test fix

--- test/controllers/api_controller_test.rb
+++ test/controllers/api_controller_test.rb
@@ -4,12 +4,12 @@ class ApiControllerTest < ActionDispatch::IntegrationTest
   test "should get error response" do
     post api_path, params: { "asd" => { "kek" => "000" } }, as: :json
     puts @response.body
-    assert_same JSON.parse(@response.body), { "error" => "JSON validation failed" }
+    assert_equal JSON.parse(@response.body), { "error" => "JSON validation failed" }
   end
 
   test "should get booking response" do
     post api_path, params: { "booking" => { "room" => "001", "date" => "01-01-2018" } }, as: :json
     puts @response.body
-    assert_not_nil (JSON.parse(@response.body))[:booked]
+    assert_not_nil (JSON.parse(@response.body)).booked
   end
 end
