cca9f0b176d09b51656955743b841f34214922b5
Mertinousse <mertinousse@gmail.com>
2018-03-28 02:04:20 +0200

    asd

--- test/controllers/api_controller_test.rb
+++ test/controllers/api_controller_test.rb
@@ -2,12 +2,14 @@ require 'test_helper'
 
 class ApiControllerTest < ActionDispatch::IntegrationTest
   test "should get error response" do
-    post api_path, params: { "asd" => { "kek" => "000" } }, as: :json
-    assert_same @response, { "error" => "JSON validation failed" }
+    post api_path, params: { "asd": { "kek": "000" } }, as: :json
+    puts @response
+    assert_same @response, { "error": "JSON validation failed" }
   end
 
   test "should get booking response" do
-    post api_path, params: { "booking" => { "room" => "001", "date" => "01-01-2018" } }, as: :json
+    post api_path, params: { "booking": { "room": "001", "date": "01-01-2018" } }, as: :json
+    puts @response
     assert_not_nil @response[:booked]
   end
 end
