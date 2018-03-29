5cbee59e59180abf5b5c6d3594a5bad306e3e96c
Mertinousse <33594576+Mertinousse@users.noreply.github.com>
2018-03-29 15:20:35 +0200

    rollback

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,14 +3,11 @@ class ApiController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def info
-    booking = Booking.new(params.require(:booking).permit(:room, :date))
-    # if booking_present? and @data.has_key? :room and @data.has_key? :date
-    if booking.valid?
-      # @data[:date] = Date.parse(@data[:date])
+    if request_valid?
+      @data[:date] = Date.parse(@data[:date])
       @res = { :booked => false }
       Booking.all.each do |b|
-        # if (b[:start] <=> @data[:date]) < 1 and (@data[:date] <=> b[:end]) < 0 then
-        if (b[:start] <=> booking.date) < 1 and (booking.date <=> b[:end]) < 0 then
+        if (b[:start] <=> @data[:date]) < 1 and (@data[:date] <=> b[:end]) < 0 then
           @res[:booked] = true
           break
         end
@@ -21,14 +18,14 @@ class ApiController < ApplicationController
     render :json => @res
   end
 
-  # private
-  # def booking_present?
-  #   begin
-  #     @data = params.require(:booking).permit(:room, :date)
-  #   rescue ActionController::ParameterMissing => e
-  #     return false
-  #   end
-  #   return true
-  # end
+  private
+  def request_valid?
+    begin
+      @data = params.require(:booking).permit(:room, :date)
+    rescue ActionController::ParameterMissing => e
+      return false
+    end
+    return @data.has_key? :room and @data.has_key? :date
+  end
 
 end
