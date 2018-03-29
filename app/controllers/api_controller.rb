d4e3e182409977c6cdefa66b3636737d30cf2050
Mertinousse <33594576+Mertinousse@users.noreply.github.com>
2018-03-29 14:44:16 +0200

    duplicated validation fix

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,11 +3,14 @@ class ApiController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def info
-    if booking_present? and @data.has_key? :room and @data.has_key? :date
-      @data[:date] = Date.parse(@data[:date])
+    booking = Booking.new(params)
+    # if booking_present? and @data.has_key? :room and @data.has_key? :date
+    if booking.valid?
+      # @data[:date] = Date.parse(@data[:date])
       @res = { :booked => false }
       Booking.all.each do |b|
-        if (b[:start] <=> @data[:date]) < 1 and (@data[:date] <=> b[:end]) < 0 then
+        # if (b[:start] <=> @data[:date]) < 1 and (@data[:date] <=> b[:end]) < 0 then
+        if (b[:start] <=> booking[:date]) < 1 and (booking[:date] <=> b[:end]) < 0 then
           @res[:booked] = true
           break
         end
