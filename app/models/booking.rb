83fc9690429e02c85415bea6e67f15822e762eaf
Mertinousse <33594576+Mertinousse@users.noreply.github.com>
2018-03-29 18:08:12 +0200

    me stupid again

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -31,7 +31,7 @@ class Booking < ActiveRecord::Base
     #     break
     #   end
     # end
-    existing_booking = Booking.where("end > :start AND start < :end AND room == :room", {
+    existing_booking = Booking.where("end > :start AND start < :end AND room = :room", {
       start: self.start, end: self.end, room: self.room
       })
     errors[:base] << "The room is already booked during this time" if existing_booking
